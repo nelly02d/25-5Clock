b3887ef03b936e254336200e8d7b72f38a4dbce8
Arnel <nelly02d@gmail.com>
2023-02-16 19:02:35 -0500

    Add a function on app.js to format seconds to time

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 function App() {
+  //initial state minutes multiply seconds = 1500 seconds
+  const [displayTime, setDisplayTime] = useState( 25 * 60 );
+
+  //a function that format time seconds to minutes, so we can have a value of 25 minutes
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    return (
+      (minutes < 10 ? '0' + minutes : minutes) +
+      ':' +
+      (seconds < 10 ? '0' + seconds : seconds)
+    );
+  };
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <div>
+        <h1>Pamodoro Clock</h1>
+      </div>
+      <h1>{formatTime(displayTime)}</h1>
+
     </div>
   );
 }
